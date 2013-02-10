43de620a9c3c089d47d5a32ec155ef9659ea20c2
Ariel Burman <arielburman@gmail.com>
2013-02-10 10:24:26 -0300

    actualizo el ejemplo de serie a la nueva version de biblioteca y Makefile

--- src/serial/main.c
+++ src/serial/main.c
@@ -6,7 +6,7 @@
 int main(void){
     DDRC |= (1<<PC3);
 
-    USART0Setup(USART_BAUDRATE, USART0_CHARSIZE_8BIT, USART0_STOP_1BIT, USART0_PARITY_DIS, USART0_MODE_ASYNC);
+    USART0Setup();
 
     while(1){
         USART0PutsP(PSTR("Hello PC!!!!\n"));
